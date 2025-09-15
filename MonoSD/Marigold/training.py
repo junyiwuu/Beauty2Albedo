d6c8e017c21ede8f7dc6e2a03aebd704d210898b
wujunyi <wujunyi00@outlook.com>
2025-09-15 14:35:57 +0100

    fix license

--- MonoSD/Marigold/training.py
+++ MonoSD/Marigold/training.py
@@ -1,3 +1,28 @@
+# An official reimplemented version of Marigold training script
+# Last modified: 2024-05-17
+#
+# Copyright 2023 Bingxin Ke, ETH Zurich. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# --------------------------------------------------------------------------
+# If you find this code useful, we kindly ask you to cite our paper in your work.
+# Please find bibtex at: https://github.com/prs-eth/Marigold#-citation
+# More information about the method can be found at https://marigoldmonodepth.github.io
+# --------------------------------------------------------------------------
+
+
+# Modified from https://github.com/prs-eth/Marigold/blob/62413d56099d36573b2de1eb8c429839734b7782/train.py, licensed under the Apache License 2.0.
+# Changes made by Junyi Wu, 2025.
 
 from diffusers import UNet2DConditionModel, AutoencoderKL, DDIMScheduler
 from transformers import CLIPTextModel, CLIPTokenizer
